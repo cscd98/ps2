52269c294e702e0a6ee21dcd469b19878720be44
LibretroAdmin <LibretroAdmin@users.noreply.github.com>
2026-06-01 03:50:25 +0000

    USB: define OHCI clock layer and IOP IRQ hook to fix link
    The previous commit added the OHCI host-controller core to the build but
    left its clock/IRQ layer undefined, breaking the link with undefined
    references to usb_get_clock(), usb_get_ticks_per_second(), usbIrq(int),
    and the g_usb_bit_time / g_usb_frame_time / g_usb_last_cycle globals.
    
    Provide them:
    
    - libretro/USB.cpp: define the clock globals and accessors (PSXCLK =
      36.864 MHz IOP clock), the s_usb_clocks / s_usb_remaining counters, and
      the OHCI instance lifecycle. USBopen/close/reset create, free and hard-
      reset the controller; USBread32/USBwrite32 dispatch to ohci_mem_read/
      write; USBasync advances the controller clock and steps frame
      boundaries. The frame/bit-time globals are filled by ohci_create().
    
    - pcsx2/IopIrq.cpp: add usbIrq(int cycles), scheduling IopEvt_USB via
      PSX_INT, mirroring the existing dev9Irq hook.
    
    Device attachment (HID keyboard/mouse instantiation) is intentionally
    still deferred; the controller links and resets cleanly but enumerates no
    devices yet. With no device attached USBasync is effectively inert.

--- libretro/USB.cpp
+++ libretro/USB.cpp
@@ -1,18 +1,133 @@
+/*  PCSX2 - PS2 Emulator for PCs
+ *  Copyright (C) 2002-2021  PCSX2 Dev Team
+ *
+ *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Lesser General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  PCSX2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with PCSX2.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdlib>
+
 #include "../pcsx2/USB/USB.h"
+#include "../pcsx2/USB/libretro-usb/USBinternal.h"
 #include "../pcsx2/SaveState.h"
 
+/* IOP clock, 36.864 MHz. */
+#define PSXCLK 36864000
+
+/* OHCI host-controller clock-timing globals. Computed inside ohci_create()
+ * from usb_get_ticks_per_second(); defined here so the controller core has
+ * storage to link against. */
+s64 g_usb_frame_time = 0;
+s64 g_usb_bit_time   = 0;
+s64 g_usb_last_cycle = 0;
+
+static OHCIState* s_qemu_ohci   = nullptr;
+static s64        s_usb_clocks  = 0;
+static s64        s_usb_remaining = 0;
+
+int usb_get_ticks_per_second(void)
+{
+	return PSXCLK;
+}
+
+s64 usb_get_clock(void)
+{
+	return s_usb_clocks;
+}
+
 void USB::CheckForConfigChanges(const Pcsx2Config& old_config) { }
+
 void USBconfigure(void) {}
-void USBinit(void) { }
-void USBasync(u32 cycles) {}
-void USBshutdown(void) { }
-void USBclose(void) {}
-void USBreset(void) {}
-bool USBopen(void) { return true; }
+
+void USBinit(void) {}
+
+void USBshutdown(void) {}
+
+bool USBopen(void)
+{
+	s_qemu_ohci = ohci_create(0x1f801600, 2);
+	if (!s_qemu_ohci)
+		return false;
+
+	s_usb_clocks     = 0;
+	s_usb_remaining  = 0;
+	g_usb_last_cycle = 0;
+	return true;
+}
+
+void USBclose(void)
+{
+	if (s_qemu_ohci)
+	{
+		free(s_qemu_ohci);
+		s_qemu_ohci = nullptr;
+	}
+}
+
+void USBreset(void)
+{
+	s_usb_clocks     = 0;
+	s_usb_remaining  = 0;
+	g_usb_last_cycle = 0;
+	if (s_qemu_ohci)
+		ohci_hard_reset(s_qemu_ohci);
+}
+
+void USBasync(u32 cycles)
+{
+	if (!s_qemu_ohci)
+		return;
+
+	s_usb_remaining += cycles;
+	s_usb_clocks    += s_usb_remaining;
+	if (s_qemu_ohci->eof_timer > 0)
+	{
+		while ((u64)s_usb_remaining >= s_qemu_ohci->eof_timer)
+		{
+			s_usb_remaining -= s_qemu_ohci->eof_timer;
+			s_qemu_ohci->eof_timer = 0;
+			ohci_frame_boundary(s_qemu_ohci);
+
+			/* Break out of the loop if the bus was stopped. */
+			if (!s_qemu_ohci->eof_timer)
+				break;
+		}
+		if ((s_usb_remaining > 0) && (s_qemu_ohci->eof_timer > 0))
+		{
+			s64 m = s_qemu_ohci->eof_timer;
+			if (s_usb_remaining < m)
+				m = s_usb_remaining;
+			s_qemu_ohci->eof_timer -= m;
+			s_usb_remaining        -= m;
+		}
+	}
+}
+
 s32 USBfreeze(FreezeAction mode, freezeData* data) { return 0; }
+
 u8 USBread8(u32 addr) { return 0; }
 u16 USBread16(u32 addr) { return 0; }
-u32 USBread32(u32 addr) { return 0; }
+
+u32 USBread32(u32 addr)
+{
+	if (!s_qemu_ohci)
+		return 0;
+	return ohci_mem_read(s_qemu_ohci, addr);
+}
+
 void USBwrite8(u32 addr, u8 value) {}
 void USBwrite16(u32 addr, u16 value) {}
-void USBwrite32(u32 addr, u32 value) {}
+
+void USBwrite32(u32 addr, u32 value)
+{
+	if (s_qemu_ohci)
+		ohci_mem_write(s_qemu_ohci, addr, value);
+}
